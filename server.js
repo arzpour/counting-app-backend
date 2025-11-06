43d6c235b2053646992e4896b76581ae8d2ca767
Setareh Arzpour <arzpourr@gmail.com>
2025-11-06 13:13:09 +0330

    Modify server file

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ mongoose
 
 app.use("/api", apiRouters);
 
-app.listen(process.env.PORT, () => {
-  console.log(`🚀 Server running on port ${process.env.PORT}`);
+app.listen(process.env.PORT, process.env.HOST, () => {
+  console.log(`🚀 Server running on port ${process.env.PORT} host ${process.env.HOST}`);
 });
