93faa156b311d0705ae04c372d7fb2808ee94688
z.arzpour <arzpourr@gmail.com>
2025-12-28 01:27:49 +0330

    Modify user login validation

--- validations/user.ts
+++ validations/user.ts
@@ -4,6 +4,11 @@ const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)/;
 
 export const userLoginValidationSchema = Joi.object({
   username: Joi.string().required(),
+  password: Joi.string().required(),
+});
+
+export const userRegistrationValidationSchema = Joi.object({
+  username: Joi.string().required(),
   password: Joi.string().required().min(8).pattern(passwordRegex),
 });
 
